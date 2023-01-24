ddb0e7dd52461cbfe5d07ba167289adf28520ed7
ShamaKung <shamaturin@gmail.com>
2023-01-25 00:11:37 +0500

    решение

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿string[] strArray = new string[5] {"Hi", "Git", "hello", "world", "Hub"};
+string[] strArrayNew = new string[strArray.Length];
+
+void SecondArray(string[] strArray, string[] strArrayNew)
+{
+    int count = 0;
+    for (int i = 0; i < strArray.Length; i++)
+    {
+    if(strArray[i].Length <= 3)
+        {
+        strArrayNew[count] = strArray[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+SecondArray(strArray, strArrayNew);
+PrintArray(strArrayNew);
